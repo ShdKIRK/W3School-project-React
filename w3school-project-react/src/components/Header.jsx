bbf4626a0495bd0c78ecfb2781f866efe5e7a9a4
ShdKIRK <185662815+ShdKIRK@users.noreply.github.com>
2025-04-20 20:45:42 +0530

    Header component added

---
+++ w3school-project-react/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles.css'
+
+export default function Header() {
+  return (
+    <div className='header'>
+    <img className='header-logo' src="./public/logo.png" alt="moviedux" />
+      <h2 className='app-subtitle'>Its MOvie time let us chill</h2>
+    </div>
+  )
+}
