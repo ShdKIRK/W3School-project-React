d05345e1892c62dab6554ce066a3f41ff43059ae
ShdKIRK <185662815+ShdKIRK@users.noreply.github.com>
2025-04-22 23:35:44 +0530

    grid card added

--- w3school-project-react/src/components/MoviesGrid.jsx
+++ w3school-project-react/src/components/MoviesGrid.jsx
@@ -18,15 +18,15 @@ export default function MoviesGrid() {
       
     }, [])
     return (
-        <div className="movie-card">
+        <div className="movies-grid">
             {
         movies.map(movie => (
             <div className="movie-card" key={movie.id}>
                 <img src={`images/${movie.image}`} alt={movie.title} />
-                    <div className="movies-card-info">
-                        <h2>{movie.title}</h2>
-                        <p>{movie.description}</p>
-                        <p>Rating: {movie.rating}</p>                        
+                    <div className="movie-card-info">
+                        <h2 className="movie-card-title">{movie.title}</h2>
+                        <p className="movie-card-genre">{movie.genre}</p>
+                        <p className="movie-card-rating" >Rating: {movie.rating}</p>                        
                     </div>
             </div>
         ))
