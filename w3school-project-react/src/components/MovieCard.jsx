7c6701fba3f5dd4eed9c8489510be95afef31ec8
ShdKIRK <185662815+ShdKIRK@users.noreply.github.com>
2025-04-26 18:03:55 +0530

    Error Handle for image

--- w3school-project-react/src/components/MovieCard.jsx
+++ w3school-project-react/src/components/MovieCard.jsx
@@ -3,9 +3,13 @@ import '../styles.css';
 
 
 export default function MovieCard({movie}) {
+
+  const handleError = (e) => {
+    e.target.src ="images/default.jpg"
+  }
   return (
     <div className="movie-card" key={movie.id}>
-      <img src={`images/${movie.image}`} alt={movie.title} />
+      <img src={`images/${movie.image}`} alt={movie.title} onError={handleError}/>
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
